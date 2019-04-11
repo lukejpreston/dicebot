4a62be2ea981e70eb8c577efcbcde5e9ac46af5d
Luke Preston <lukep@ceh.ac.uk>
2019-04-11 13:21:41 +0100

    Updates

---
+++ precache-manifest.b7cce012f8a64035badeee516176ef35.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cd196c2bee6d34b79e6",
+    "url": "/dicebot/static/js/main.3fa29db7.chunk.js"
+  },
+  {
+    "revision": "c03c533185d7300242f2",
+    "url": "/dicebot/static/js/runtime~main.85fe13b8.js"
+  },
+  {
+    "revision": "9f5ec8bd8e4096c31462",
+    "url": "/dicebot/static/js/2.6cab279c.chunk.js"
+  },
+  {
+    "revision": "fa15e9b2904e5a46ce33d432c584fd9f",
+    "url": "/dicebot/index.html"
+  }
+];
